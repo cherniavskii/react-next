2d703954baef6bb9520644d4efee52bd4fc92173
Andrew <andrew.cherniavskii@gmail.com>
2018-04-27 22:17:00 +0200

    add material-ui

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ WIP. Base for creating SSR React apps.
 ## ToDo:
 - [x] add `react` and `react-dom`
 - [x] add `next.js`
-- [ ] add `material-ui`
+- [x] add `material-ui`
 - [x] add `webpack-bundle-analyzer`
 - [ ] add `redux` and `redux-logic`
 - [ ] add `jest`
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,52 @@
-export default () => (
-  <h1>React-next</h1>
-)
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import withRoot from '../src/withRoot';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  flex: {
+    flex: 1,
+  },
+  content: {
+    padding: 16,
+  },
+};
+
+class Index extends React.Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="title" color="inherit" className={classes.flex}>
+              React-next
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Typography className={classes.content} variant="subheading">
+          You can edit <code>pages/index.js</code> now and app will automatically refresh :)
+        </Typography>
+      </div>
+    );
+  }
+}
+
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRoot(withStyles(styles)(Index));
