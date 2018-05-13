047f1c399b6372b678055e303435592b804fb24a
Andrew <andrew.cherniavskii@gmail.com>
2018-05-13 12:27:26 +0200

    add redux-logic

--- README.md
+++ README.md
@@ -7,7 +7,8 @@ WIP. Base for creating SSR React apps.
 - [x] add `next.js`
 - [x] add `material-ui`
 - [x] add `webpack-bundle-analyzer`
-- [ ] add `redux` and `redux-logic`
+- [x] add `redux` and `redux-logic`
+- [ ] add `axios`
 - [ ] add `jest`
 - [ ] add `enzyme`
 - [x] add `eslint`
--- src/store.js
+++ src/store.js
@@ -1,11 +1,17 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogicMiddleware } from 'redux-logic';
 import rootReducer from './rootReducer';
+import logic from '../services/logic';
 
 export default function (initialState = {}) {
   return createStore(
     rootReducer,
     initialState,
-    composeWithDevTools(),
+    composeWithDevTools((
+      applyMiddleware((
+        createLogicMiddleware(logic, {})
+      ))
+    )),
   );
 }
