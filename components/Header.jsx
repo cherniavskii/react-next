3e53099284044d103331c10e7530024d6f83cedc
Andrew <andrew.cherniavskii@gmail.com>
2018-05-13 13:35:26 +0200

    replace menu icon in Header with Home button redirecting to home page

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,14 +3,17 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import IconButton from 'material-ui/IconButton';
 import Typography from 'material-ui/Typography';
-import MenuIcon from '@material-ui/icons/Menu';
+import HomeIcon from '@material-ui/icons/Home';
+import Link from 'next/link';
 
 const Header = () => (
   <AppBar position="static">
     <Toolbar>
-      <IconButton color="inherit" aria-label="Menu">
-        <MenuIcon />
-      </IconButton>
+      <Link href="/" passHref>
+        <IconButton color="inherit" aria-label="Home" component="a">
+          <HomeIcon />
+        </IconButton>
+      </Link>
       <Typography variant="title" color="inherit" style={{ flex: 1 }}>
         React-next
       </Typography>
